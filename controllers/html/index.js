2f3780bea3e4bd8fc07414aef89bba33a2d7fab5
Andreas Kaiafas <akaiafas526@gmail.com>
2022-03-28 22:14:16 -0400

    update to html folders index.js file

--- controllers/html/index.js
+++ controllers/html/index.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 
-const allPosts = require('./allPosts.js');
-const postRoutes = require('./post-routes');
-const commentRoutes = require('./comment-routes');
+const allPostsRoutes = require('./allPosts.js');
+const postTagRoutes = require('./postsByTag.js');
+const postUserRoutes = require('./postsByUser');
 
-router.use('/user', allPosts);
-router.use('/post', postRoutes);
-router.use('/comment', commentRoutes);
-
+router.use('/html', allPostsRoutes);
+// -----------ASK GROUP IN MORNING-----------
+// router.use('/post', postTagRoutes);
+// router.use('/comment', postUserRoutes);
+// ------------------------------------------
 module.exports = router;
