7fefb1677faf12e4198339aac35e9eb4895441ec
Brian Halpin <brian.t.halpin@gmail.com>
2022-03-28 21:51:59 -0400

    Add get route for posts by user

--- controllers/html/allPosts.js
+++ controllers/html/allPosts.js
@@ -52,4 +52,22 @@ router.get('/tag/:id',  async (req, res) => {
     }
 });
 
+router.get('/user/:id',  async (req, res) => {
+    try {
+        const posts = await Post.findAll({
+            include: [{model:Comment,include:[{model:User}]},{model:User},{model:Tag}],
+        
+            where:{
+                userId:req.params.id
+            }
+        });
+        const post = posts.map(post=>post.get({plain:true}));
+        console.log(post)
+        // res.render('SOME_VIEW',post)
+        res.status(200).json(post)
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
 module.exports = router;
