be25884377604119c3d8d8a54827e664ed88bb31
Nick Broglio <nickrbroglio@gmail.com>
2022-03-29 14:51:33 -0400

    registerjs file

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,9 +2,10 @@ const router = require("express").Router();
 const { User } = require("../../models");
 
 router.post("/", async (req, res) => {
+  console.log('something')
   try {
     const userData = await User.create(req.body);
-
+    console.log('spmetingneelse')
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
