6346bb95434e15e7f82b956853bd5ee6c37010e9
Andreas Kaiafas <akaiafas526@gmail.com>
2022-03-30 13:51:11 -0400

    update to commentData seeds file

--- seeds/commentData.js
+++ seeds/commentData.js
@@ -2,69 +2,81 @@ const { Comment } = require('../models');
 
 const commentData = [
   {
+    userId: 2,
+    postId: 1,
+    text: 'What an amazing animal! hope you had a great trip.',
+    
+  },
+  {
     userId: 1,
-    postId: 1,
-    text: 'Some really good comment',
-    
-  },
-  {
-    userId: 10,
-    postId: 1,
-    text: '111Some really good comment',
-    
-  },
-  {
-    userId: 3,
     postId: 2,
-    text: '222Some really good comment',
+    text: 'Yum! such an artsy photo!',
     
   },
   {
-    userId: 9,
+    userId: 4,
     postId: 3,
-    text: '333Some really good comment',
+    text: 'wow...my brain hurts',
     
   },
   {
     userId: 8,
-    postId: 5,
-    text: '444Some really good comment',
-    
-  },
-  {
-    userId: 2,
-    postId: 7,
-    text: '555Some really good comment',
+    postId: 3,
+    text: 'such a cool photo',
     
   },
   {
     userId: 3,
+    postId: 4,
+    text: 'what a beautiful picture!',
+    
+  },
+  {
+    userId: 5,
+    postId: 4,
+    text: 'the water is so blue!',
+    
+  },
+  {
+    userId: 6,
+    postId: 5,
+    text: 'have fun!',
+    
+  },
+  {
+    userId: 2,
+    postId: 5,
+    text: 'stunning photo!',
+    
+  },
+  {
+    userId: 10,
+    postId: 6,
+    text: 'you are so brave!',
+    
+  },
+  {
+    userId: 11,
+    postId: 7,
+    text: 'family is the best!',
+    
+  },
+  {
+    userId: 1,
+    postId: 8,
+    text: 'where can I get a pair of glasses like that?',
+    
+  },
+  {
+    userId: 8,
+    postId: 9,
+    text: 'kebabs are my favorite',
+    
+  },
+  {
+    userId: 2,
     postId: 10,
-    text: '666Some really good comment',
-    
-  },
-  {
-    userId: 7,
-    postId: 3,
-    text: '777Some really good comment',
-    
-  },
-  {
-    userId: 1,
-    postId: 1,
-    text: '88Some really good comment',
-    
-  },
-  {
-    userId: 1,
-    postId: 1,
-    text: '999Some really good comment',
-    
-  },
-  {
-    userId: 1,
-    postId: 1,
-    text: '1010Some really good comment',
+    text: 'they did such a great job rebuilding the World Trade Center!',
     
   },
   
