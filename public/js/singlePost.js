fbd64292dd21f92a1c9319a3c7f31fc4b4a8ff54
Nick Broglio <nickrbroglio@gmail.com>
2022-03-30 22:00:05 -0400

    styles and edit post

--- public/js/singlePost.js
+++ public/js/singlePost.js
@@ -1,23 +1,40 @@
-
-
 const deleteBtn = document.querySelector('.deleteModalBtn');
+const editBtn = document.querySelector('.editModalBtn');
+
+const commentBtn = document.querySelector('.commentModalBtn');
+const commentBoxEl = document.querySelector('.newComment');
+
 const post = document.querySelector('.post');
 
 async function deletePost()  {
-    
-    // const id = post.dataset.postId
     const id = post.getAttribute('data-postId')
-    console.log(post)
-    console.log(id)
-    // fetch delete post route
     const data = await fetch(`/api/post/${id}`, {
         method: 'DELETE',
         headers: { 'Content-Type': 'application/json' },
     })
-    // console.log(fetch)
+    if (data.ok){
+        document.location.assign('/');
+    }
+    else {
+        console.log('ERROR')
+    }
 }
-
-
-
-
 deleteBtn.addEventListener('click', deletePost)
+
+
+// ask at start of class
+// async function editPost() {
+//     const id = post.getAttribute('data-postId')
+//     const data = await fetch (`/api/posts/${id}`, {
+//         method:'PUT',
+//         headers: { 'Content-Type': 'application/json' },
+//     })
+//     if(data.ok){
+//         document.location.assign('singlePost');
+//     } else{
+//         console.log('ERROR')
+//     }
+// }
+// editBtn.addEventListener('click', editPost)
+
+
