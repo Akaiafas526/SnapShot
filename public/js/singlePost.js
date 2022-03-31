6fd0821c749879b09b598831e90a820324319cc6
Andreas Kaiafas <akaiafas526@gmail.com>
2022-03-31 11:58:59 -0400

    edit post function works on postRoutes.js file

--- public/js/singlePost.js
+++ public/js/singlePost.js
@@ -1,10 +1,11 @@
 const deleteBtn = document.querySelector('.deleteModalBtn');
 const editBtn = document.querySelector('.editModalBtn');
-const formEl = document.querySelector(".comment-form");
+const cFormEl = document.querySelector(".comment-post-form");
+const eFormEl = document.querySelector(".edit-post-form")
 const post = document.querySelector('.post');
 
 
-
+// function that deletes post
 async function deletePost()  {
     const id = post.getAttribute('data-postId')
     const data = await fetch(`/api/post/${id}`, {
@@ -21,27 +22,33 @@ async function deletePost()  {
 deleteBtn.addEventListener('click', deletePost)
 
 
-// ask at start of class
-// async function editPost() {
-//     const id = post.getAttribute('data-postId')
-//     const data = await fetch (`/api/posts/${id}`, {
-//         method:'PUT',
-//         headers: { 'Content-Type': 'application/json' },
-//     })
-//     if(data.ok){
-//         document.location.assign('singlePost');
-//     } else{
-//         console.log('ERROR')
-//     }
-// }
-// editBtn.addEventListener('click', editPost)
+
+eFormEl.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const title = document.querySelector('.post-title').value
+  const description = document.querySelector('.description-box').value
+
+  const id = post.getAttribute('data-postId')
+  const response = await fetch(`/api/post/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({
+       title,
+       description
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+  if (response.ok) {
+    document.location.reload();
+  } else {
+    console.log("Unable to edit post");
+  }
+});
 
 
 
 
 
-
-formEl.addEventListener("submit", async (e) => {
+cFormEl.addEventListener("submit", async (e) => {
   e.preventDefault();
   const response = await fetch("/api/comment", {
     method: "POST",
