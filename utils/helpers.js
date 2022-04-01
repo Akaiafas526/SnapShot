b2b277e61bcbd1ac71474aeb693bc2f3824f2d34
Brian Halpin <brian.t.halpin@gmail.com>
2022-04-01 14:00:39 -0400

    Add style to post button

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,13 +2,13 @@ module.exports = {
   format_date: (date) => {
     console.log(date,'DATE   ASDASD ASDADS')
     let hour = date.getHours()+1;
-    let timeOfDay;
-    if(hour>12){
-      hour-=12
-      timeOfDay = 'PM'
-    } else {
-      timeOfDay = 'AM'
-    }
-    return `${hour}:${date.getMinutes()}${timeOfDay} - ${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+    // let timeOfDay;
+    // if(hour>12){
+    //   hour-=12
+    //   timeOfDay = 'PM'
+    // } else {
+    //   timeOfDay = 'AM'
+    // }
+    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
   },
 };
